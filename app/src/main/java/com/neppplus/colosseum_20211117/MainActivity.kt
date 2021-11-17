edcd41b228bd33aa704974a96ba82659f016b5c3
nepp-plus <nepp.plus@gmail.com>
2021-11-17 15:17:33 +0900

    로그인 버튼 눌리면 -> email / pw 변수에 저장

--- app/src/main/java/com/neppplus/colosseum_20211117/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/MainActivity.kt
@@ -19,6 +19,17 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        binding.btnLogin.setOnClickListener {
+
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPw = binding.edtPassword.text.toString()
+
+//            서버에서 이메일 / 비번이 맞는 계정인지? 로그인 요청
+
+
+
+        }
+
     }
 
     override fun setValues() {
