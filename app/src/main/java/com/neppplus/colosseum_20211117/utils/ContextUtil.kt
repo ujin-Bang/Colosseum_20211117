f2e65d86bbb3cd97a958d1e9eba134d688f0ceaa
nepp-plus <nepp.plus@gmail.com>
2021-11-18 12:21:37 +0900

    ContextUtil : 토큰값 조회 기능 추가

--- app/src/main/java/com/neppplus/colosseum_20211117/utils/ContextUtil.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/utils/ContextUtil.kt
@@ -25,6 +25,17 @@ class ContextUtil {
 
 //        token getter 함수
 
+        fun getToken( context: Context ) : String {
+
+//            1. 메모장 열기
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+//            2. 저장된 토큰값을 리턴
+            return pref.getString(TOKEN, "")!!
+
+
+        }
+
     }
 
 }
