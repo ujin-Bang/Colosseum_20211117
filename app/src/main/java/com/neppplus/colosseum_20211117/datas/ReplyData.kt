7024e378386039f974c7ce30ed05298ef7b19930
nepp-plus <nepp.plus@gmail.com>
2021-11-19 12:09:33 +0900

    댓글 - 선택 진영 파싱 / 표시

--- app/src/main/java/com/neppplus/colosseum_20211117/datas/ReplyData.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/datas/ReplyData.kt
@@ -9,6 +9,10 @@ class ReplyData {
 
     var writer = UserData()  // 사용자데이터가 들어올거라고 명시.
 
+//    어느 진영에 대한 댓글?
+    var selectedSide = SideData()
+
+
     companion object {
 
         fun getReplyDataFromJson( jsonObj:  JSONObject ) : ReplyData {
@@ -20,6 +24,9 @@ class ReplyData {
 
             val userObj = jsonObj.getJSONObject("user")
             replyData.writer = UserData.getUserDataFromJson(userObj)
+
+            val sideObj = jsonObj.getJSONObject("selected_side")
+            replyData.selectedSide =  SideData.getSideDataFromJson(sideObj)
 
             return replyData
 
