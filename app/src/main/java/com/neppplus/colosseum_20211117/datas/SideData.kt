c2084d656a156f1371cd52c00566133140a0a3e6
nepp-plus <nepp.plus@gmail.com>
2021-11-19 10:07:07 +0900

    선택 진영 데이터 표현용 클래스   - JSON > SideData 변환 함수 추가

---
+++ app/src/main/java/com/neppplus/colosseum_20211117/datas/SideData.kt
@@ -0,0 +1,29 @@
+package com.neppplus.colosseum_20211117.datas
+
+import org.json.JSONObject
+
+class SideData {
+
+    var id = 0
+    var title = ""
+    var voteCount = 0
+
+    companion object {
+
+        fun getSideDataFromJson( jsonObj: JSONObject ) : SideData {
+
+            val sideData = SideData()
+
+//            기본값 -> jsonObj 를 이용해서 파싱된 값으로 대입.
+            sideData.id = jsonObj.getInt("id")
+            sideData.title = jsonObj.getString("title")
+            sideData.voteCount =  jsonObj.getInt("vote_count")
+
+
+            return  sideData
+
+        }
+
+    }
+
+}
