8e5df80c30d0eeecbd38df1fce0b7433db66117c
nepp-plus <nepp.plus@gmail.com>
2021-11-19 11:47:30 +0900

    댓글 추가파싱 중 - 사용자 데이터 발견  - 데이터 클래스 추가 + 파싱 함수 추가

---
+++ app/src/main/java/com/neppplus/colosseum_20211117/datas/UserData.kt
@@ -0,0 +1,27 @@
+package com.neppplus.colosseum_20211117.datas
+
+import org.json.JSONObject
+
+class UserData {
+
+    var id = 0
+    var email = ""
+    var nickname = ""
+
+    companion object {
+
+        fun getUserDataFromServer( jsonObj : JSONObject ) : UserData {
+
+            val userData = UserData()
+
+            userData.id = jsonObj.getInt("id")
+            userData.email = jsonObj.getString("email")
+            userData.nickname = jsonObj.getString("nick_name")
+
+            return userData
+        }
+
+    }
+
+
+}
