228c4f49135144251314081b5b169a6cf4a5d9d0
nepp-plus <nepp.plus@gmail.com>
2021-11-19 11:10:25 +0900

    댓글 데이터 표현용 클래스 추가  - 파싱 함수 같이 추가

---
+++ app/src/main/java/com/neppplus/colosseum_20211117/datas/ReplyData.kt
@@ -0,0 +1,25 @@
+package com.neppplus.colosseum_20211117.datas
+
+import org.json.JSONObject
+
+class ReplyData {
+
+    var id = 0
+    var content = ""
+
+    companion object {
+
+        fun getReplyDataFromJson( jsonObj:  JSONObject ) : ReplyData {
+
+            val replyData = ReplyData()
+
+            replyData.id = jsonObj.getInt("id")
+            replyData.content = jsonObj.getString("content")
+
+            return replyData
+
+        }
+
+    }
+
+}
