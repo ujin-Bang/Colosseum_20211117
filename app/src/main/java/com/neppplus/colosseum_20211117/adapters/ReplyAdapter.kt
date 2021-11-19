7aba09f3d86408fab382482c71c497705e780ed8
nepp-plus <nepp.plus@gmail.com>
2021-11-19 14:46:04 +0900

    파싱한 작성일시 -> 재가공 (format)해서 댓글 목록에 뿌려주기

--- app/src/main/java/com/neppplus/colosseum_20211117/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/adapters/ReplyAdapter.kt
@@ -11,6 +11,7 @@ import com.bumptech.glide.Glide
 import com.neppplus.colosseum_20211117.R
 import com.neppplus.colosseum_20211117.datas.ReplyData
 import com.neppplus.colosseum_20211117.datas.TopicData
+import java.text.SimpleDateFormat
 
 class ReplyAdapter(
     val mContext: Context,
@@ -35,12 +36,17 @@ class ReplyAdapter(
         val txtReplyContent = row.findViewById<TextView>(R.id.txtReplyContent)
         val txtWriterNickname = row.findViewById<TextView>(R.id.txtWriterNickname)
         val txtSelectedSide = row.findViewById<TextView>(R.id.txtSelectedSide)
+        val txtCreatedAt = row.findViewById<TextView>(R.id.txtCreatedAt)
 
         txtReplyContent.text = data.content
 
         txtWriterNickname.text = data.writer.nickname
 
         txtSelectedSide.text = "(${data.selectedSide.title})"
+
+        val sdf = SimpleDateFormat("yyyy/MM/dd a h시 m분")
+
+        txtCreatedAt.text = sdf.format(data.createdAt.time)
 
         return row
 
