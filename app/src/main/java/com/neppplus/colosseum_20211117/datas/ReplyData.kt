5f6886e0fffd6e60a624f43d101bd1611a358506
nepp-plus <nepp.plus@gmail.com>
2021-11-19 11:50:53 +0900

    댓글 파싱 중 - 사용자 데이터 (작성자)도 같이 파싱

--- app/src/main/java/com/neppplus/colosseum_20211117/datas/ReplyData.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/datas/ReplyData.kt
@@ -7,6 +7,8 @@ class ReplyData {
     var id = 0
     var content = ""
 
+    var writer = UserData()  // 사용자데이터가 들어올거라고 명시.
+
     companion object {
 
         fun getReplyDataFromJson( jsonObj:  JSONObject ) : ReplyData {
@@ -15,6 +17,9 @@ class ReplyData {
 
             replyData.id = jsonObj.getInt("id")
             replyData.content = jsonObj.getString("content")
+
+            val userObj = jsonObj.getJSONObject("user")
+            replyData.writer = UserData.getUserDataFromJson(userObj)
 
             return replyData
 
