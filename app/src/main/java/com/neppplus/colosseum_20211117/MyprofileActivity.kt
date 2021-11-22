850219c43387f06d04ff710e86892cd0927d6c93
ujin-Bang <qkddnwls14@gmail.com>
2021-11-22 10:11:59 +0900

    로그아웃 - 경고창 띄우기

--- app/src/main/java/com/neppplus/colosseum_20211117/MyprofileActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/MyprofileActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.colosseum_20211117
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -15,6 +16,21 @@ class MyprofileActivity : BaseActivity {
     override fun setupEvents() {
 
 
+        binding.btnLogout.setOnClickLister {
+
+//            찐짜 로그아웃 할건지? 확인
+             val alert = AlerDialog.Buler(mContext)
+            alert.setTitle("로그아웃 확인")
+            alert.setMassage("정말 로그아웃 하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+
+//                실제 로그아웃 처리
+
+            })
+            alert.setNegativeButton("취소",null)
+            alert.show()
+
+        }
     }
 
     override fun setValues() {
