75f5eb05c19c83d8c568e17e09b9f503db6ab313
ujin-Bang <qkddnwls14@gmail.com>
2021-11-22 11:50:48 +0900

    넘겨 받은 데이터 활용 - UI에 반영   - 클래스의 여러 하위 클래스들도 활용

--- app/src/main/java/com/neppplus/colosseum_20211117/ViewReplyDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/ViewReplyDetailActivity.kt
@@ -3,13 +3,17 @@ package com.neppplus.colosseum_20211117
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.colosseum_20211117.databinding.ActivityViewReplyDetailBinding
+import com.neppplus.colosseum_20211117.datas.ReplyData
 
 class ViewReplyDetailActivity : BaseActivity {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        lateinit var binding : ActivityViewReplyDetailBinding
+        lateinit var
+
+                super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_reply_detail)
 
-        lateinit var binding : ActivityViewReplyDetailBinding
+
         setValues()
         setupEvents()
     }
@@ -19,5 +23,11 @@ class ViewReplyDetailActivity : BaseActivity {
     }
 
     override fun setValues() {
+
+        mReplyData = intent.getSerializableExtra("reply") as ReplyData
+
+        binding.txtWriterNickName.text = mReplyData.writer.nikname
+
+        binding.txtContent.text = mReplyData.content
     }
 }
